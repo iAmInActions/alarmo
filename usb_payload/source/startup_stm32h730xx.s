8b2a265826f12deeeaf6fe9e3902409ba27b5d58
GaryOderNichts <12049776+GaryOderNichts@users.noreply.github.com>
2024-11-16 22:59:51 +0100

    usb_payload: Properly link against libc

--- usb_payload/source/startup_stm32h730xx.s
+++ usb_payload/source/startup_stm32h730xx.s
@@ -94,8 +94,7 @@ LoopFillZerobss:
   bcc FillZerobss
 
 /* Call static constructors */
-    @ TODO alarmo
-    @ bl __libc_init_array
+  bl __libc_init_array
 /* Call the application's entry point.*/
   bl  main
   bx  lr
